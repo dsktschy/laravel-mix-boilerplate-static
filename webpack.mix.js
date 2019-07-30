9c54e8e3828c2f411fc18998dc7635b971c12d09
dai <chamestica@gmail.com>
2019-07-30 22:43:45 +0900

    Use EJS instead of Pug as template

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,7 @@ require('laravel-mix-copy-watched')
 require('laravel-mix-eslint')
 require('laravel-mix-stylelint')
 require('laravel-mix-imagemin')
-mix.pug = require('@dsktschy/laravel-mix-pug')
+require('laravel-mix-ejs')
 
 const srcRelativePath =
   (process.env.MIX_SRC_RELATIVE_PATH || 'resources')
@@ -85,7 +85,8 @@ mix
     // https://github.com/JeffreyWay/laravel-mix/issues/1053
     files: [
       `${distRelativePath}/assets/**/*`,
-      `${distRelativePath}/**/*.html`
+      `${distRelativePath}/**/*.html`,
+      `${distRelativePath}/**/*.php`
     ],
     https:
       process.env.MIX_BROWSER_SYNC_HTTPS_CERT &&
@@ -104,32 +105,29 @@ mix
   // Second argument is source map type. Note that several types don't output map for CSS
   // https://webpack.js.org/configuration/devtool/#devtool
   .sourceMaps(false, 'inline-cheap-module-source-map')
-  .pug(
-    `${srcRelativePath}/views/**/[!_]*.pug`,
+  .ejs(
+    `${srcRelativePath}/views`,
     distRelativePath,
     {
-      // Path to directory that contains JSON or YAML
-      seeds: srcRelativePath,
-      // Variables and functions
-      locals: {
-        // Function for cache busting
-        mix: (filePath = '') => basePath + filePath + '?id=' + Date.now(),
-        // Function to create path for SVG sprite, according to NODE_ENV
-        // Requires path to sprite SVG file and ID
-        // In development, if SVG is included in pug,
-        // injection of changes such as CSS by BrowserSync is prevented
-        // Because svg-spritemap-webpack-plugin reacts for all changes,
-        // and it causes pug recompile and browser reloading
-        svgSprite: (filePath = '', id = '') =>
-          process.env.NODE_ENV === 'production' ? id : basePath + filePath + id
-      },
-      // Base directory
-      excludePath: `${srcRelativePath}/views`,
-      // Options for Pug
-      pug: {
-        // Required to include partials with root relative path
-        basedir: `${srcRelativePath}/views`
-      }
+      // Imitate mix function of Laravel Mix
+      mix: (filePath = '') => process.env.NODE_ENV === 'production'
+        ? basePath + filePath + '?id=' + Date.now()
+        : basePath + filePath,
+      // Function to create path for SVG sprite, according to NODE_ENV
+      // Requires path to sprite SVG file and ID
+      // In development, if SVG is included in EJS,
+      // injection of changes such as CSS by BrowserSync is prevented
+      // Because svg-spritemap-webpack-plugin reacts for all changes,
+      // and it causes EJS recompile and browser reloading
+      svgSprite: (filePath = '', id = '') => process.env.NODE_ENV === 'production'
+        ? id
+        : basePath + filePath + id
+    },
+    {
+      outputFunctionName: 'echo',
+      root: `${srcRelativePath}/views`,
+      base: `${srcRelativePath}/views`,
+      partials: `${srcRelativePath}/views/partials`
     }
   )
 
@@ -156,7 +154,7 @@ if (process.env.NODE_ENV === 'production') {
       fs.removeSync(`${distRelativePath}/mix-manifest.json`)
     })
     // It's difficult handle public/mix-manifest.json from static pages
-    // Can use function of Pug instead of PHP, to set parameter for cache busting
+    // Can use function of EJS instead of PHP, to set parameter for cache busting
     // .version()
 }
 
