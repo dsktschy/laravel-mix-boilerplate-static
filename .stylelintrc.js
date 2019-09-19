1417f230350cb180b124bc677454bf1eb5a47005
dai <chamestica@gmail.com>
2019-09-19 18:16:33 +0900

    Use stylelint-config-recommended-scss

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,3 @@
 module.exports = {
-  extends: 'stylelint-config-recommended',
-  plugins: [
-    'stylelint-scss'
-  ],
-  rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true
-  }
+  extends: 'stylelint-config-recommended-scss'
 }
